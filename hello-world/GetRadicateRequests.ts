2e8843275b22ddce36fbcafd840f3300f70d588d
Daniel Rumbo <danielrumbo@MacBook-Pro-de-Daniel.local>
2024-11-18 14:44:39 -0500

    se agrega paquete de NPM al proyecto

--- hello-world/GetRadicateRequests.ts
+++ hello-world/GetRadicateRequests.ts
@@ -4,11 +4,11 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 export const getRadicateRequests = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         const radicadoRequests = [
-            { id: 1, description: 'Bogota-1234', date_update: '1-11-2024', date_request: '1-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
-            { id: 1, description: 'Bogota-1235', date_update: '30-11-2024', date_request: '30-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
-            { id: 1, description: 'Bogota-1267', date_update: '28-11-2024', date_request: '28-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
-            { id: 1, description: 'Bogota-1223', date_update: '14-11-2024', date_request: '14-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
-            { id: 1, description: 'Bogota-1236', date_update: '9-11-2024', date_request: '9-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
+            { id: 1, description: 'Bogota-1234-2024', date_update: '1-11-2024', date_request: '1-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
+            { id: 1, description: 'Bogota-1235-2024', date_update: '30-11-2024', date_request: '30-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
+            { id: 1, description: 'Bogota-1267-2024', date_update: '28-11-2024', date_request: '28-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
+            { id: 1, description: 'Bogota-1223-2024', date_update: '14-11-2024', date_request: '14-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
+            { id: 1, description: 'Bogota-1236-2024', date_update: '9-11-2024', date_request: '9-11-2024', status: 'proceso', resp_user: 'Daniel Rumbo', creator_user: 'Pepito perez' },
         ];
 
         return {
